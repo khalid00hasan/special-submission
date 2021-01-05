ea0454665a5a5ba2f6ad9dfb93bd1e12f893453c
unknown <khalidhasan842@gmail.com>
2021-01-05 23:48:58 +0600

    submition

---
+++ CRUD_operation_Train/CRUD_operation_Train/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace CRUD_operation_Train
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txttraId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TrainInfo = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtlast = new System.Windows.Forms.Label();
+            this.txtcurren = new System.Windows.Forms.Label();
+            this.txttraName = new System.Windows.Forms.TextBox();
+            this.txtstart = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TrainInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(85, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txttraId
+            // 
+            this.txttraId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttraId.Location = new System.Drawing.Point(275, 15);
+            this.txttraId.Name = "txttraId";
+            this.txttraId.Size = new System.Drawing.Size(173, 24);
+            this.txttraId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Train id";
+            // 
+            // TrainInfo
+            // 
+            this.TrainInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TrainInfo.Location = new System.Drawing.Point(8, 255);
+            this.TrainInfo.Name = "TrainInfo";
+            this.TrainInfo.Size = new System.Drawing.Size(780, 183);
+            this.TrainInfo.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(656, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(497, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(275, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tra Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(135, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Starting Station";
+            // 
+            // txtlast
+            // 
+            this.txtlast.AutoSize = true;
+            this.txtlast.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlast.Location = new System.Drawing.Point(135, 132);
+            this.txtlast.Name = "txtlast";
+            this.txtlast.Size = new System.Drawing.Size(79, 18);
+            this.txtlast.TabIndex = 9;
+            this.txtlast.Text = "last station";
+            // 
+            // txtcurren
+            // 
+            this.txtcurren.AutoSize = true;
+            this.txtcurren.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcurren.Location = new System.Drawing.Point(135, 170);
+            this.txtcurren.Name = "txtcurren";
+            this.txtcurren.Size = new System.Drawing.Size(102, 18);
+            this.txtcurren.TabIndex = 10;
+            this.txtcurren.Text = "current station";
+            // 
+            // txttraName
+            // 
+            this.txttraName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttraName.Location = new System.Drawing.Point(275, 49);
+            this.txttraName.Name = "txttraName";
+            this.txttraName.Size = new System.Drawing.Size(173, 24);
+            this.txttraName.TabIndex = 11;
+            // 
+            // txtstart
+            // 
+            this.txtstart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtstart.Location = new System.Drawing.Point(275, 91);
+            this.txtstart.Name = "txtstart";
+            this.txtstart.Size = new System.Drawing.Size(173, 24);
+            this.txtstart.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(275, 126);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 24);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(275, 164);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 24);
+            this.textBox5.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtstart);
+            this.Controls.Add(this.txttraName);
+            this.Controls.Add(this.txtcurren);
+            this.Controls.Add(this.txtlast);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TrainInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txttraId);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Train info";
+            ((System.ComponentModel.ISupportInitialize)(this.TrainInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txttraId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView TrainInfo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtlast;
+        private System.Windows.Forms.Label txtcurren;
+        private System.Windows.Forms.TextBox txttraName;
+        private System.Windows.Forms.TextBox txtstart;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ CRUD_operation_Train/CRUD_operation_Train/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace CRUD_operation_Train
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-LNO9I49\SQLEXPRESS;Initial Catalog=TrainInfo;Integrated Security=True");
+        private void Form1_Load(object sender,EventArgs e)
+        {
+            GetTrainInfo();
+        }
+
+        private void GetTrainInfo()
+        {
+            
+            SqlCommand cmd = new SqlCommand("select * from TrainTB", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            dt.Load(sdr);
+
+            con.Close();
+
+            TrainInfo.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Isvalid())
+
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO TrainTB VALUES (@TrainName, @StartingStation,@StopingStation,@currentStation)", con );
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@TrainName", txttraName.Text);
+                cmd.Parameters.AddWithValue("@StartingStation", txtstart.Text);
+                cmd.Parameters.AddWithValue("@StopingStation", txtlast .Text);
+                cmd.Parameters.AddWithValue("@currentStation", txtcurren.Text);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                GetTrainInfo();
+            }
+        }
+        private bool Isvalid()
+        {
+            if (txttraId.Text ==string.Empty)
+            {
+                MessageBox.Show("Name is required", "failled", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txttraName.Clear();
+            txtstart.Clear();
+            
+        }
+    }
+}
